22e1dd743f8913b6f282688c265f1b9b841e616f
Nick D'Ademo <nickdademo@gmail.com>
2016-01-09 18:31:18 +0100

    Update copyright year.

--- src/ProcessingThread.cpp
+++ src/ProcessingThread.cpp
@@ -6,7 +6,7 @@
 /*                                                                      */
 /* Nick D'Ademo <nickdademo@gmail.com>                                  */
 /*                                                                      */
-/* Copyright (c) 2012-2015 Nick D'Ademo                                 */
+/* Copyright (c) 2012-2016 Nick D'Ademo                                 */
 /*                                                                      */
 /* Permission is hereby granted, free of charge, to any person          */
 /* obtaining a copy of this software and associated documentation       */
